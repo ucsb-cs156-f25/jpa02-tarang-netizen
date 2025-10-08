f0857656aa27b5f42cb20a60b36f95d61ba2ffb1
tarang-netizen <tarang@ucsb.edu>
2025-10-08 16:16:59 -0700

    Update Developer.java

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -18,7 +18,7 @@ public class Developer {
     public static String getName() {
         // had already changed name to mine. changed it back so tests pass
         
-        return "Chris G.";
+        return "Tarang S.";
     }
 
     /**
