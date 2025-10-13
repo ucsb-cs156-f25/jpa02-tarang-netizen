ac36ddcd59486ffb1dbfc649821c5dc7eeec6c56
tarang-netizen <tarang@ucsb.edu>
2025-10-13 15:04:27 -0700

    Update HelloController.java

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -51,20 +51,15 @@ public class HelloController {
 
     @GetMapping("/team")
     public Team team() {
-        return Developer.getTeam();
-    }
-
-  @GetMapping("/team")
-    public Team getTeam() {
-    Team team = new Team();
     team.addMember("JOSHUA");
     team.addMember("DYLAN");
     team.addMember("TARANG");
     team.addMember("ADAM YITAO");
     team.addMember("CHENCHANG");
     team.addMember("TERRY");
-    return team;
-}
+        return Developer.getTeam();
+    }
+
 
 
 }
